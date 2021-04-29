f37927077634b3b2464c180e72f02e4f2cf316c0
finvansant <ffparke@gmail.com>
2021-04-29 09:43:31 -0400

    Add Novelty Page

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ from page_introduction import page_introduction
 from page_clusters import page_clusters
 from page_velocity import page_velocity
 from page_team import page_team
+from page_novelty import page_novelty
 
 # Sidebar
 st.sidebar.image('./images/cornell_tech_logo.png')
+
+st.sidebar.title("Navigation")
 
 sidebar_pages = {
     "Introduction": page_introduction,
     "Explore Clusters": page_clusters,
     "Velocity": page_velocity,
+    "Novelty": page_novelty,
     "Team": page_team
 }
-
-st.sidebar.title("Navigation")
 
 page = st.sidebar.radio("Select:", tuple(sidebar_pages.keys()))
 
---
+++ page_novelty.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def page_novelty():
+
+    st.text('page_novelty')
+
+    return
