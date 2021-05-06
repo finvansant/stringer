51bfcdb5f821e0c82704e0fabcf3387d81822295
finvansant <ffparke@gmail.com>
2021-05-06 16:49:15 -0400

    Velocity Tables

--- page_velocity.py
+++ page_velocity.py
@@ -6,9 +6,28 @@ def page_velocity():
 
     st.text('page_velocity')
 
-    df_velocity = pd.read_csv('data/velocity_page/clustersEvolution.csv', index_col=0)
+    st.subheader('Explore Velocity')
+    st.write("")
 
-    st.subheader('Work in Progress!')
+
+    # State changes of clusters
+    st.write("Top keywords for each cluster:")
+    df_clustersEvolution = pd.read_csv('data/velocity_page/clustersEvolution.csv', index_col=0)
+    st.write(df_clustersEvolution)
+
+
+
+    # Percentage change within evolved clusters
+    st.write("Percentage change within evolved clusters:")
+    df_percentageChange = pd.read_csv('data/velocity_page/evolvedClustersPercentChange.csv', index_col=0)
+    st.write(df_percentageChange)
+
+    # Statistics regarding new clusters
+    st.write("Statistics regarding new clusters")
+    df_newClusterStats = pd.read_csv('data/velocity_page/newClustersStats.csv', index_col=0)
+    st.write(df_newClusterStats)
+
+
 
 
 
